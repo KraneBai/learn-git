a5ea7b25b30b0525ebdd296592f001cb0f8a432c
KraneBai <206678552@qq.com>
2019-05-17 09:28:19 +0800

    Updates

---
+++ precache-manifest.ecd9a82b93c0fc8dabf9cf4cb60c3277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18119c32794355f1a27c9d3a91ebdf00",
+    "url": "/KraneBai/learn-git/index.html"
+  },
+  {
+    "revision": "aa4fb836b53194f1eb99",
+    "url": "/KraneBai/learn-git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d7f116452cd9c9cdd18c",
+    "url": "/KraneBai/learn-git/static/js/2.c9db5bbf.chunk.js"
+  },
+  {
+    "revision": "aa4fb836b53194f1eb99",
+    "url": "/KraneBai/learn-git/static/js/main.07961759.chunk.js"
+  },
+  {
+    "revision": "7be60c28b38f9923229f",
+    "url": "/KraneBai/learn-git/static/js/runtime~main.f4a3f4b2.js"
+  }
+]);
